1a5c993f8271f545a28484455b1d52327cfac1c0
Víctor <Victor@MacBook-Pro-de-Victor.local>
2022-02-12 09:49:59 +0100

    General update

--- Code/config.R
+++ Code/config.R
@@ -4,10 +4,24 @@
 
 #### Setup ####
 
-zlim <- 1.5
-meanzlim <- 2
-rm_artifacts <- TRUE
+## Study
+project <- "Premature vs Normal Aging"
+species <- "Mus musculus" # Mus musculus Homo sapiens Sus scrofa
+databases <- ""
+rel_to <- "sample_mean" # control_mean sample_mean
+fdr_quant <- 0.01 # False discovery rate for quantification
 
+## Parameters
+zlims <- seq(1, 3, 0.1)
+alphas <- c(0.01, 0.05)
+sat_lim <- 3 # Saturation limit for plots
+
+## Artifact Proteins
+rm_artifacts <- TRUE # Remove artifact proteins
+exclude_pattern <- c("krt", "keratin", "hba", "Hbb", "hemoglobin", "myoglobin", "haptoglobin", "albumin", "trypsin", "serpin", "complement c")
+exclude_text <- "keratin contaminants, trypsin, and several major serum proteins (albumin, globins, serpins and complement factors)"
+
+## Name of fields in datasets
 prot_field <- "Protein"
 np_field <- "Np"
 
--- Code/functions_general.R
+++ Code/functions_general.R
@@ -4,12 +4,52 @@
 #### Load functions ####
 
 libraries <- c("data.table", 
+               "openxlsx",
                "BiocManager")
 
 bioc_libraries <- c("org.Mm.eg.db", 
                     "GOstats")
 
-get_repo_libraries <- function(libraries, bioconductor = FALSE) {
+
+#### New functions ####
+
+#' Get Libraries
+#' 
+#' @description Installs and loads all required libraries.
+#' 
+#' @param general_libraries Vector of libraries in CRAN.
+#' @param bioc_libraries Vector of libraries in Bioconductor. Default is NULL.
+#' 
+#' @details BiocManager is installed and loaded if required before the installation
+#' of Bioconductor libraries.
+#' 
+#' @author Victor Fanjul (2021-08-01)
+
+get_libraries <- function(general_libraries, 
+                          bioc_libraries = NULL) {
+  
+  if (!is.null(bioc_libraries)) {
+    general_libraries <- unique(c(general_libraries, "BiocManager"))
+  }
+  
+  get_repo_libraries(general_libraries)
+  if (!is.null(bioc_libraries)) get_repo_libraries(bioc_libraries, TRUE)
+}
+
+
+
+#' Get Libraries from Repository
+#' 
+#' @description Installs and loads required libraries from specific repository 
+#' (CRAN or Bioconductor).
+#' 
+#' @param libraries Vector of libraries.
+#' @param bioconductor Whether libraries are from Bioconductor. Default is FALSE.
+#' 
+#' @author Victor Fanjul (2021-08-01)
+
+get_repo_libraries <- function(libraries, 
+                               bioconductor = FALSE) {
   
   new_lbs <- setdiff(libraries, rownames(installed.packages()))
   unloaded_lbs <- setdiff(libraries, (.packages()))
@@ -25,26 +65,72 @@ get_repo_libraries <- function(libraries, bioconductor = FALSE) {
 }
 
 
-get_libraries <- function(general_libraries, 
-                          bioc_libraries = NULL,
-                          print_loaded = FALSE
-                          ) {
+
+#' Write Excel File
+#' 
+#' @description Exports proteomics dataset to Excel with conditional formatting.
+#' 
+#' @param dt Proteomics dataset.
+#' @param file File path and name.
+#' @param worksheet Name of Excel sheet.
+#' @param sample_cols Vector of z column names.
+#' @param p_value_cols Vector of p value column names.
+#' @param change_cols Vector of change column names.
+#' @param np_col Number of peptides (Np) column.
+#' @param sat_lim Color saturation z threshold. Default is 3.
+#' @param change_colors Vector with color scale for z and change columns. 
+#' Default is c("dodgerblue", "white", "red").
+#' @param enhance_colors Vector with color scale for Np and p value columns.
+#'  Default is c("limegreen", "white").
+#' 
+#' @details 
+#' 
+#' # Required libraries:
+#' * openxlsx
+#' 
+#' @author Victor Fanjul (2021-12-05)
+
+write_excel <- function(dt, file, worksheet, sample_cols, p_value_cols, 
+                        change_cols, np_col, 
+                        sat_lim = 3,
+                        change_colors = c("dodgerblue", "white", "red"),
+                        enhance_colors = c("limegreen", "white")) {
   
-  if (!is.null(bioc_libraries)) {
-    general_libraries <- unique(c(general_libraries, "BiocManager"))
-  }
+  data_fields <- names(dt)
+  wb <- createWorkbook()
+  addWorksheet(wb, worksheet)
+  writeData(wb, worksheet, dt, withFilter = TRUE)
   
-  get_repo_libraries(general_libraries)
-  if (!is.null(bioc_libraries)) get_repo_libraries(bioc_libraries, TRUE)
+  conditionalFormatting(wb, worksheet,
+                        cols = which(data_fields %in% sample_cols), 
+                        rows = 1:nrow(dt) + 1,
+                        style = change_colors,
+                        rule = c(-sat_lim, 0, sat_lim),
+                        type = "colourScale")
   
-  if(print_loaded) {
-    message("Loaded libraries are:")
-    print((.packages()))
-  }
+  conditionalFormatting(wb, worksheet,
+                        cols = which(data_fields %in% p_value_cols), 
+                        rows = 1:nrow(dt) + 1,
+                        style = enhance_colors,
+                        rule = c(0, 0.05),
+                        type = "colourScale")
+  
+  conditionalFormatting(wb, worksheet,
+                        cols = which(data_fields %in% change_cols), 
+                        rows = 1:nrow(dt) + 1,
+                        style = change_colors,
+                        rule = c(-1, 0, 1),
+                        type = "colourScale")
+  
+  conditionalFormatting(wb, worksheet,
+                        cols = which(data_fields %in% np_col), 
+                        rows = 1:nrow(dt) + 1,
+                        style = enhance_colors,
+                        rule = c(1, 2),
+                        type = "colourScale")
+  
+  saveWorkbook(wb, file, TRUE)
 }
 
-get_libraries(libraries, bioc_libraries)
 
 
-
-#### New functions ####
