5eb439c32e5a5cb6c07d3783d5ec760c25f27e5d
Víctor <Victor@MacBook-Pro-de-Victor.local>
2022-07-02 18:28:23 +0200

    - Add time counter to run script

--- Code/run_proteomics_results_analysis.R
+++ Code/run_proteomics_results_analysis.R
@@ -1,3 +1,4 @@
-
+time0 <- Sys.time()
 source("Code/config.R")
 rmarkdown::render("Code/protein_level.Rmd", output_file = paste0("../", outpuroute, project, " protein_results.html"))
+Sys.time() - time0
