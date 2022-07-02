9d11014a8194844212857e1407582aabe974698f
Víctor <Victor@MacBook-Pro-de-Victor.local>
2022-07-02 18:27:04 +0200

    - Update config script

--- Code/config.R
+++ Code/config.R
@@ -6,7 +6,7 @@
 
 ## Study
 author <- "Víctor Fanjul, PhD"
-project <- "Premature vs Normal Aging"
+project <- "Premature vs Normal Aging (2)"
 species <- "Mus musculus" # Mus musculus Homo sapiens Sus scrofa
 databases <- ""
 rel_to <- "sample_mean" # control_mean sample_mean
@@ -16,15 +16,16 @@ fdr_quant <- 0.01 # False discovery rate for quantification
 zlims <- seq(1, 3, 0.1)
 alphas <- c(0.01, 0.05)
 sat_lim <- 3 # Saturation limit for plots
+adj.pvalue <- TRUE # Adjust p value
 
 ## Artifact Proteins
 rm_artifacts <- TRUE # Remove artifact proteins
 exclude_pattern <- c("krt", "keratin", "hba", "Hbb", "hemoglobin", "myoglobin", "haptoglobin", "albumin", "trypsin", "serpin", "complement c")
 exclude_text <- "keratin contaminants, trypsin, and several major serum proteins (albumin, globins, serpins and complement factors)"
 
-## Name of fields in datasets
-prot_field <- "Protein"
-np_field <- "Np"
+## Name of cols in datasets
+prot_col <- "Protein"
+np_col <- "Np"
 
 
 #### File paths ####
